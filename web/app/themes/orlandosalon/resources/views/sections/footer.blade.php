5614aeb814e3dd33df14c6cf6d2ff1097ca2b8da
lljb3 <lljb3@lljb3.com>
2024-12-28 22:56:36 -0600

    Check home page tablet styling

--- web/app/themes/orlandosalon/resources/views/sections/footer.blade.php
+++ web/app/themes/orlandosalon/resources/views/sections/footer.blade.php
@@ -6,10 +6,10 @@
             </div>
             <div class="col-lg-5 offset-lg-1" id="site-map">
                 <div class="row">
-                    <div class="col-lg-4">
+                    <div class="col-lg-4 col-md-6">
                         {!! wp_nav_menu(['menu' => 'Site Map Menu', 'menu_class' => 'nav', 'menu_id' => 'site-map-menu', 'echo' => false]) !!}
                     </div>
-                    <div class="col-lg-8">
+                    <div class="col-lg-8 col-md-6">
                         {!! wp_nav_menu(['menu' => 'Policy Menu', 'menu_class' => 'nav', 'menu_id' => 'policies-menu', 'echo' => false]) !!}
                     </div>
                 </div>
@@ -22,7 +22,7 @@
         <div class="row mt-5" id="bottom-content">
             <div class="col-lg-12">
                 <div class="row">
-                    <div class="col-lg-6 col-6" id="credits"><?php echo '©' . date('Y') . ' Orlando Salon. <br class="d-block d-lg-none">All rights reserved.'; ?></div>
+                    <div class="col-lg-6 col-6" id="credits"><?php echo '©' . date('Y') . ' Orlando Salon. <br class="d-block d-md-none">All rights reserved.'; ?></div>
                     <div class="col-lg-6 col-6 text-end" id="to-top"><a href="#top" id="scroll-to-top">Back To Top<i class="fas fa-arrow-up"></i></a></div>
                 </div>
             </div>
